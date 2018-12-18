00e03199af6e193355a7b1833d54fa02f8b090cf
davidecesarano <davide.cesarano@unipegaso.it>
2018-12-18 11:37:27 +0100

    Update example
    Signed-off-by: davidecesarano <davide.cesarano@unipegaso.it>

--- example/index.php
+++ example/index.php
@@ -1,30 +1,37 @@
 <?php 
 
-    require 'vendor/autoload.php';
+    require __DIR__.'/../vendor/autoload.php';
 
     class Person
     {
-      public function getName()
-      {
-        return 'David';
-      }
+        public function getName()
+        {
+            return 'David';
+        } 
     }
 
     class Hello
     {
-      public $name;
+        public $name;
 
-      public function __construct(Person $person)
-      {
-        $this->name = $person->getName();
-      }
+        public function __construct(Person $person)
+        {
+            $this->name = $person->getName();
+        }
 
-      public function getHello()
-      {
-        return 'Hello '.$this->name;
-      }
+        public function getHello()
+        {
+            return 'This is a reflection! Hello '.$this->name;
+        }
     }
 
     $container = new \Embryo\Container\Container;
-    $hello = $container->get('Hello');
-    echo $hello->getHello(); // Hello David
+    
+    $container->set('myService', function(){
+        return 'This is myService!<br>';
+    });
+
+    $hello = $container->reflection('Hello');
+    
+    echo $container->get('myService');
+    echo $hello->getHello();
--- example/service.php
+++ example/service.php
@@ -1,6 +1,6 @@
 <?php 
 
-    require 'vendor/autoload.php';
+    require __DIR__.'/../vendor/autoload.php';
     
     use Embryo\Container\Container;
     use Embryo\Container\ServiceProvider;
